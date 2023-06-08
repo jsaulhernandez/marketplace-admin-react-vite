6b5d4eb0fec6579126ea4eb489811f90556bfa3a
Sony <jsaulhernandez98@gmail.com>
2023-06-08 12:17:19 -0600

    tag: adding config for paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@root": resolve(__dirname, "./src"),
+      "@components": resolve(__dirname, "./src/components"),
+      "@pages": resolve(__dirname, "./src/pages"),
+      "@helpers": resolve(__dirname, "./src/helpers"),
+      "@contexts": resolve(__dirname, "./src/context"),
+      "@hooks": resolve(__dirname, "./src/hooks"),
+      "@layout": resolve(__dirname, "./src/layout"),
+      "@interfaces": resolve(__dirname, "./src/interfaces"),
+      "@utils": resolve(__dirname, "./src/utils"),
+      "@reducers": resolve(__dirname, "./src/reducers"),
+      "@constants": resolve(__dirname, "./src/constants"),
+    },
+  },
+  plugins: [
+    react(),
+    {
+      name: "singleHMR",
+      handleHotUpdate({ modules }) {
+        modules.map((m) => {
+          m.importedModules = new Set();
+          m.importers = new Set();
+        });
+
+        return modules;
+      },
+    },
+  ],
+});
