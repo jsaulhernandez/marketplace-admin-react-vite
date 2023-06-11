ff0b62846bf522bfa9cd8017af1c41b43da6df84
Sony <jsaulhernandez98@gmail.com>
2023-06-11 13:31:26 -0600

    tag: refactoring imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
-import 'moment/dist/locale/es';
-import './index.css';
-
-import { ConfigProvider } from 'antd';
-import es_ES from 'antd/es/locale/es_ES';
-import moment from 'moment';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
+import { ConfigProvider } from 'antd';
+import es_ES from 'antd/es/locale/es_ES';
+
+import moment from 'moment';
+import 'moment/dist/locale/es';
+
 import App from './App.tsx';
+import './index.css';
 
 moment.locale('es');
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <BrowserRouter>
