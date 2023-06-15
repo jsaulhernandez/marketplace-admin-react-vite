4e9c86eb2505455f40d3937de70908c35272e117
Sony <jsaulhernandez98@gmail.com>
2023-06-15 17:32:55 -0600

    tag: adding new constants

--- src/constants/Constants.constants.ts
+++ src/constants/Constants.constants.ts
@@ -4,3 +4,8 @@ export type NotificationType = 'success' | 'info' | 'warning' | 'error';
 export type ModalActionsType = 'success' | 'error' | 'info' | 'confirm';
 export type TypeButton = 'primary' | 'secondary' | 'link' | 'danger';
 export type UserActions = 'get' | 'save' | 'update' | 'delete';
+export const UnitsMeasurementStorageSize = Object.freeze([
+    { key: 'MB', value: 'Megabyte' },
+    { key: 'GB', value: 'Gigabyte' },
+    { key: 'TB', value: 'Terabyte' },
+]);
